bf082d70ab762195f9b6ee4f73e22976f591ee4c
twetto <franky85@hotmail.com.tw>
2019-09-02 17:57:30 +0800

    hotfix different number of iq/iz

--- src/main.cpp
+++ src/main.cpp
@@ -15,26 +15,31 @@ int main()
     char filename[] = "iq_output_Number.txt";
     iq_network network_iq;
     iz_network network_iz;
-    int num_neurons = network_iq.num_neurons();
-    FILE** fp_iq = (FILE**) malloc(sizeof(FILE*) * num_neurons);
-    FILE** fp_iz_v = (FILE**) malloc(sizeof(FILE*) * num_neurons);
-    FILE** fp_iz_u = (FILE**) malloc(sizeof(FILE*) * num_neurons);
+    int iq_num_neurons = network_iq.num_neurons();
+    int iz_num_neurons = network_iz.num_neurons();
+    FILE** fp_iq = (FILE**) malloc(sizeof(FILE*) * iq_num_neurons);
+    FILE** fp_iz_v = (FILE**) malloc(sizeof(FILE*) * iz_num_neurons);
+    FILE** fp_iz_u = (FILE**) malloc(sizeof(FILE*) * iz_num_neurons);
 
     srand((unsigned) time(NULL));
 
 
-    for(i = 0; i < num_neurons; i++) {
+    for(i = 0; i < iq_num_neurons; i++) {
         sprintf(filename, "iq_output_%d.txt", i);
         fp_iq[i] = fopen(filename, "w");
+    }
+    for(i = 0; i < iz_num_neurons; i++) {
         sprintf(filename, "iz_output_v_%d.txt", i);
         fp_iz_v[i] = fopen(filename, "w");
         sprintf(filename, "iz_output_u_%d.txt", i);
         fp_iz_u[i] = fopen(filename, "w");
     }
     
-    for(i = 0; i < num_neurons; i++) {
-        network_iz.set_biascurrent(i, 4);
+    for(i = 0; i < iq_num_neurons; i++) {
         network_iq.set_biascurrent(i, 4);
+    }
+    for(i = 0; i < iz_num_neurons; i++) {
+        network_iz.set_biascurrent(i, 4);
     }
     for(j = 0; j < 2000; j++) {
         network_iz.send_synapse();
@@ -42,9 +47,11 @@ int main()
         network_iq.send_synapse();
         network_iq.printfile(fp_iq);
     }
-    for(i = 0; i < num_neurons; i++) {
-        network_iz.set_biascurrent(i, 0);
+    for(i = 0; i < iq_num_neurons; i++) {
         network_iq.set_biascurrent(i, 0);
+    }
+    for(i = 0; i < iz_num_neurons; i++) {
+        network_iz.set_biascurrent(i, 0);
     }
     for(j = 0; j < 2000; j++) {
         network_iz.send_synapse();
@@ -96,8 +103,10 @@ int main()
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Izhikevich took %f seconds to execute.\n", cpu_time_used);
     */
-    for(i = 0; i < num_neurons; i++) {
+    for(i = 0; i < iq_num_neurons; i++) {
         fclose(fp_iq[i]);
+    }
+    for(i = 0; i < iz_num_neurons; i++) {
         fclose(fp_iz_v[i]);
         fclose(fp_iz_u[i]);
     }
