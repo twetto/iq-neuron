78f6751df8fa84f880d1b8dbcb6b7a25751af2bf
twetto <franky85@hotmail.com.tw>
2020-09-24 15:22:15 +0800

    Add num_threads debug line

--- src/iq_network.cpp
+++ src/iq_network.cpp
@@ -318,6 +318,7 @@ void iq_network::set_num_threads(int num_threads)
 {
     _num_threads = num_threads;
     omp_set_num_threads(num_threads);
+    printf("number of threads: %d\n", _num_threads);
     return;
 }
 
